a796f5ab75cf24304cdc56bc8369d11c957ef818
tharuka <tharuka@gmail.com>
2026-03-29 16:54:11 +1100

    backup

--- web/components/ui/select.tsx
+++ web/components/ui/select.tsx
@@ -79,7 +79,7 @@ const SelectContent = React.forwardRef<
       <SelectScrollUpButton />
       <SelectPrimitive.Viewport
         className={cn(
-          'p-1',
+          'max-h-96 overflow-y-auto p-1',
           position === 'popper' && 'w-full min-w-[var(--radix-select-trigger-width)]',
           viewportClassName
         )}
